6ef5a970df135a56dd3de8dcceba6a6157f714fe
LuRuiDeng <1373793524@qq.com>
2016-06-14 16:21:54 +0800

    changes log

---
+++ poject/scripts/a.js
@@ -0,0 +1,14 @@
+﻿$(function(){
+	$(" .more").hover(function(){
+		$(this).find('a').addClass('hover_a');
+		$(this).find('.listBox').stop().slideDown(400);
+	},function(){
+		$(this).find('a').removeClass('hover_a');
+		$(this).find('.listBox').stop().slideUp(600);
+	});
+	$(".more .listBox li").hover(function(){
+		$(this).addClass('on').siblings().removeClass('on');
+	},function(){
+		$(this).removeClass('on');/*针对鼠标离开ul区域的情况*/
+	});
+});
